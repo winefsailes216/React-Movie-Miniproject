f7ddefd1bdf680c162a1ff8b8ea87b6dba470233
unknown <winefsailes216@gmail.com>
2022-08-18 23:54:23 +0800

    adding movie carousel from api

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='absolute flex items-center justify-between p-4 z[100] w-full'>
-        <h1 className='text-red-600 text-4xl font-semibold cursor-pointer'>MovieWorld</h1>
+        <h1 className='text-red-600 text-4xl font-semibold cursor-pointer z-10'>MovieWorld</h1>
         <div>
             <button className='text-white pr-4'>Sign In</button>
             <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button>
