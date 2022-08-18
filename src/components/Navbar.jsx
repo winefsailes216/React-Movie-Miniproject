0806871b48501604b710d98e28d3b391bbe9622b
unknown <winefsailes216@gmail.com>
2022-08-19 00:39:03 +0800

    added env and context

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className='absolute flex items-center justify-between p-4 z[100] w-full'>
         <h1 className='text-red-600 text-4xl font-semibold cursor-pointer z-10'>MovieWorld</h1>
         <div>
-            <button className='text-white pr-4'>Sign In</button>
+            <button className='text-white pr-4 cursor-pointer'>Sign In</button>
             <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button>
         </div>
     
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,6 +16,14 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID
 };
 
+// apiKey: "AIzaSyC_uTK94SCpEMLxB7Ui4e5vS8F9LGABbAM",
+//   authDomain: "movieworld-react.firebaseapp.com",
+//   projectId: "movieworld-react",
+//   storageBucket: "pmovieworld-react.appspot.com",
+//   messagingSenderId: "427773683589",
+//   appId: "1:427773683589:web:e431f2758ae5dbd90606ec",
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app)
